97a0f54a7b9f6f50ff6dcdf3c4fcac703b778586
Gunil <gunil.na1@gmail.com>
2020-08-07 11:53:51 +0200

    added suggestions

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
 import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -24,6 +25,17 @@ const Container = styled.div`
   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
   width: 100%;
   display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const SugContainer = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(45deg, #fd723a, #d754ab);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
   color: white;
@@ -57,6 +69,15 @@ const Poster = styled.div`
   background-position: center center;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
   let { id } = useParams();
   id = parseInt(id);
@@ -66,15 +87,25 @@ export default () => {
   });
   console.log(data);
   return (
-    <Container>
-      <Column>
-        <Title>{loading ? "Loading..." : data.movie.title}</Title>
-        <Subtitle>
-          {data?.movie?.language} · {data?.movie?.rating}
-        </Subtitle>
-        <Description>{data?.movie?.description_full}</Description>
-      </Column>
-      <Poster bg={data?.movie?.medium_cover_image}></Poster>
-    </Container>
+    <>
+      <Container>
+        <Column>
+          <Title>{loading ? "Loading..." : data.movie.title}</Title>
+          <Subtitle>
+            {data?.movie?.language} · {data?.movie?.rating}
+          </Subtitle>
+          <Description>{data?.movie?.description_full}</Description>
+        </Column>
+        <Poster bg={data?.movie?.medium_cover_image}></Poster>
+      </Container>
+      <SugContainer>
+        <Title>Suggestions</Title>
+        <Movies>
+          {data?.suggestions?.map((s) => (
+            <Movie key={s.id} id={s.id} bg={s.medium_cover_image} />
+          ))}
+        </Movies>
+      </SugContainer>
+    </>
   );
 };
